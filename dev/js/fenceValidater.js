250a8a40917ec12ffcf23b4b927c338a24066cb1
Marquis Massie <massiemarquis@gmail.com>
2018-04-13 19:16:38 -0700

    Add fence demo website

---
+++ dev/js/fenceValidater.js
@@ -0,0 +1,8 @@
+/*
+File: fenceValidater.js
+Author: Marquis Massie
+Author's Email: massiemarquis@gmail.com
+Descritpion: Library of functions to validate user input
+*/
+
+//Restrict user to 
---
+++ dev/service-map.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+		<!-- Only use source files during development. Please comment out, use demo HTML copy, or download PHP version to display completed work.
+		<link rel="stylesheet" href="js/bootstrap-4.0.0-dist/css/bootstrap-grid.min.css">
+		<script src="js/jquery-3.3.1.js"></script>
+		<script src="js/bootstrap-4.0.0-dist/js/bootstrap.js"></script> -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4">
+					<figure>
+						<figcaption>Company Logo</figcaption>
+					</figure>
+				</div>
+				<div class="col-md-4"><h1>Company Slogan</h1></div>
+				<div class="col-md-4"><p>Call to action</p></div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="container">
+						<nav class="navbar nav">
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">AboutUs</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Service Area</a></li>
+								<li><a href="#">ContactUs</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
